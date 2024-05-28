28055e92a0757809ec152ba7103853b39c8265d8
Daniel Henk <henk.daniel98@gmail.com>
2024-05-28 23:20:46 +0200

    fix spawn coin

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,10 @@
 extends Node
 
-var score = 0
-@onready var score_label = $ScoreLabel
-
-func add_point():
-	score += 1
-	score_label.text = "You collected " + str(score) + " coins."
-	print(score)
+# Issue: When reloading scene the score_label 
+#var score = 0
+#@onready var score_label = get_node("/root/Game/%ScoreLabel")
+#
+#func add_point():d
+	#score += 1
+	#score_label.text = "You collected " + str(score) + " coins."
+	#print(score)
--- scripts/question_block.gd
+++ scripts/question_block.gd
@@ -1,11 +1,23 @@
 extends StaticBody2D
 
 @onready var animation_player = $AnimationPlayer
+const COIN = preload("res://scenes/coin.tscn")
+@onready var coins = $"../Coins"
+@onready var game = $".."
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func spawn_coiny():
+	print("coini")
+	var instance = COIN.instantiate()
+	instance.request_ready()
+	#coins.add_child(instance)
+	add_child(instance)
+	#game.add_child(instance)
+
 func _on_area_2d_body_entered(body):
 	print("collider")
+	spawn_coiny()
 	animation_player.play("explode")
